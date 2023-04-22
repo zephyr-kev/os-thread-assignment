3dd40745238f45199ad440ca325ecd7741f02cc3
Kevin Sinanan <70339291+zephyr-kev@users.noreply.github.com>
2023-04-21 21:46:44 -0500

    Q2 Complete, start Q3

--- Q2_2_sem.cpp
+++ Q2_2_sem.cpp
@@ -3,28 +3,38 @@
 #include <semaphore.h>
 using namespace std;
 
-sem_t sem;
+sem_t sem1, sem2;
 
 void runMeFirst() {
     cout << "Run me first" << endl;
-    sem_post(&sem);
+    sem_post(&sem1);
 }
 
 void runMeSecond() {
-    sem_wait(&sem);
+    sem_wait(&sem1);
     cout << "I run second" << endl;
+    sem_post(&sem2);
+}
+
+void runMeThird() {
+    sem_wait(&sem2);
+    cout << "I run third" << endl;
 }
 
 int main() {
-    sem_init(&sem, 0, 0);
+    sem_init(&sem1, 0, 0);
+    sem_init(&sem2, 0, 0);
 
     thread t1(runMeFirst);
     thread t2(runMeSecond);
+    thread t3(runMeThird);
 
     t1.join();
     t2.join();
+    t3.join();
 
-    sem_destroy(&sem);
+    sem_destroy(&sem1);
+    sem_destroy(&sem2);
 
     return 0;
 }
---
+++ Q3.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <thread>
+#include <semaphore.h>
+using namespace std;
+
